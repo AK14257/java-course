985a72b8d8845c3d1861e4a55d5d8aacb444cdb3
AK14257 <93402337+AK14257@users.noreply.github.com>
2024-02-19 18:00:36 +0000

    if else

---
+++ IfElseDemo.java
@@ -0,0 +1,16 @@
+public class IfElseDemo{
+    public static void main(String args[]){
+        int x=20;
+        if(x>10){
+            System.out.println("the given number x is greater than 10");
+        }
+        else if(x>10){
+            System.out.println("the given number x is less than 10");
+        }
+        else{
+            System.out.println("the given number is equal");
+        }
+        System.out.println("final destination");
+
+        }
+ }
---
+++ IfElseTest.java
@@ -0,0 +1,17 @@
+class IfElseTest{
+    public static void main(String[] args){
+        int x=-8;
+        if(x>0){
+            System.out.println("the number is positive");
+
+        }
+        else if(x<0){
+            System.out.println("the number is negative");
+
+
+        }
+        else{
+            System.out.println("the number is zero");
+        }
+        }
+    }
